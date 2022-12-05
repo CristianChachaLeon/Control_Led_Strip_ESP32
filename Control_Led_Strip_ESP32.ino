b68a966ac9f192b92b3adc236a36bc042c47f131
Cristian Chacha <cristianchacha@gmail.com>
2022-12-05 10:31:07 +0100

    Add functionality TCP/ delete UDP conection

--- Control_Led_Strip_ESP32.ino
+++ Control_Led_Strip_ESP32.ino
@@ -1,20 +1,14 @@
-// version 0.1 Hardcode num leds, no configuration through the program, prototype
+// Test TCP case use to perform comunication
 
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
-#include <WiFiUDP.h>
+//#include <WiFi.h>
 #include <FastLED.h>
-
-
 
 #define BTN_CHANGE_WIFI_CONFIG 12
 
-
 WiFiManager wm;
+WiFiServer wifiServer(8090);
 
-WiFiUDP UDP;
-unsigned int localPort = 8888;
-unsigned int remotePort = 8889;
-#define UDP_TX_PACKET_MAX_SIZE 50
 #define MAX_BUFFER_SAFE 10
 #define SIZE_MONO_QUEUE 50
 #define SIZE_STEREO_QUEUE 40
@@ -24,9 +18,13 @@ unsigned int remotePort = 8889;
 #define NUM_LEDS_R 4    // change
 #define DATA_PIN_L 22   // change
 #define DATA_PIN_R 23   // change
+
 CRGB leds_L[NUM_LEDS_L];  // change
 CRGB leds_R[NUM_LEDS_R];  // change
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE] = {};
+
+char data_TCPIP[14]; //#FFEECC255\0
+
+
 
 struct LedStripConfig {
   char Mode[10];
@@ -50,6 +48,7 @@ QueueHandle_t queue_mono;
 QueueHandle_t queue_stereo;
 
 xSemaphoreHandle binSemaphore;
+
 enum state {
   Init = 0,
   Configuration,
@@ -59,7 +58,7 @@ enum state {
 enum sub_state_init {
   Wifi_connect = 0,
   Change_wifi,
-  UDP_conection,
+  TCP_conection,
   Inactive
 };
 
@@ -74,229 +73,147 @@ enum sub_state_init Init_State = Wifi_connect;
 enum sub_state_led_control Strip_State = Mono;
 
 int messagesWaiting;
+
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(115200);
-  Serial.println("Inicializacion");
+  Serial.println("CONTROL LED TCP IP protocol");
+
   queue_config = xQueueCreate( 5, sizeof( struct LedStripConfig * ) );
+
   pinMode(BTN_CHANGE_WIFI_CONFIG, INPUT_PULLUP);
   binSemaphore = xSemaphoreCreateBinary();
+
   FastLED.addLeds<NEOPIXEL, DATA_PIN_L>(leds_L, NUM_LEDS_L);
   FastLED.addLeds<NEOPIXEL, DATA_PIN_R>(leds_R, NUM_LEDS_R);
+
   for (int i = 0; i < NUM_LEDS_L; i++) {
     leds_L[i] = CRGB(120, 20, 100);
   }
   for (int i = 0; i < NUM_LEDS_R; i++) {
     leds_R[i] = CRGB(255, 0, 0);
   }
+
   FastLED[0].showLeds(10);
   FastLED[1].showLeds(10);
-  //FastLED.show();
+
+  memset(data_TCPIP, '\0', sizeof(data_TCPIP));
+
+
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  //
   if (digitalRead(BTN_CHANGE_WIFI_CONFIG) == LOW) {
     Central_State = Init;
     Init_State = Change_wifi;
   }
 
   switch (Central_State) {
-    case Init:
+    case Init:  // State Init
       switch (Init_State) {
         case Wifi_connect:
-          Serial.println("Estado: Init --> Wifi connect");// Estado con leds
-          bool res;
-          res = wm.autoConnect("AutoConnectAP", "password"); // password protected ap
-          if (!res) {
-            Serial.println("Failed to connect");
-          } else {
-            Serial.println("ESP32 : CONNECT ! :)");
-            Init_State = UDP_conection;
-          }
+
+          if (Conect_Wifi())
+            Init_State = TCP_conection;
           break;
         case Change_wifi:
 
           break;
-        case UDP_conection:
-          Serial.println("UDP INIT");
-          delay(3000);
-          if (ConnectUDP())
-            Init_State = Inactive;
+        case TCP_conection:
+          wifiServer.begin();
+          Serial.print("Servidor iniciado\nEsperando cliente, ESP32-IP: ");
+          Serial.println(WiFi.localIP());
+          Init_State = Inactive;
+
           break;
         case Inactive:
-          //listen UDP
-          int packetSize = UDP.parsePacket();
-          if (packetSize) {
-            // read the packet into packetBufffer
-            UDP.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
-            /*Serial.println();
-              Serial.print("Received packet of size ");
-              Serial.print(packetSize);
-              Serial.print(" from ");
-              Serial.print(UDP.remoteIP());
-              Serial.print(":");
-              Serial.println(UDP.remotePort());
-              Serial.print("Payload: ");
-              Serial.write((uint8_t*)packetBuffer, (size_t)packetSize);
-              Serial.println();
-              Serial.println((char *)packetBuffer);
-              char * init;
-              init = strtok(packetBuffer, ":");
-              Serial.println((char *)init);
-              init = strtok(NULL, " ");
-              Serial.println((char *)init);
-              Serial.println((char *)packetBuffer);*/
-            if (Is_config_queue(packetBuffer)) {
-              Serial.println((char *)packetBuffer);
-              struct LedStripConfig Config_Strip;
-              strcpy(Config_Strip.Mode, "Mono"); // Data hardcode!!!
-
-              load_config_in_queue(&Config_Strip);
-
-              memset(packetBuffer, '\0', UDP_TX_PACKET_MAX_SIZE);
-              Central_State = Configuration;  //Change state to Configuration
-            }
-
-
-          }
-
+          WiFiClient client = wifiServer.available();
+          ListenClient(&client, &queue_config);
           break;
       }
       break;
-    case Configuration:
-      Serial.println("State: Configuration");
+
+    case Configuration:// State Config
       struct LedStripConfig Config_Strip_read;
       read_config_queue(&Config_Strip_read);
-
-      if (!strcmp(Config_Strip_read.Mode, "Mono")) {
-        Serial.println("Configuracion : Mono");
-        xTaskCreate(TaskLedMono, "LED MODE MONO", 1024, NULL, 2, NULL);
-        queue_mono = xQueueCreate( SIZE_MONO_QUEUE, sizeof( struct Led_Mono ) );
-        if (queue_mono == NULL) {
-          Serial.println("Error: Queue mono not created");
-        }
-
-        if (queue_stereo != NULL) {
-          vQueueDelete(queue_stereo);
-          Serial.println("Successfull : Delete Queue Stereo");
-        }
-      } else if (!strcmp(Config_Strip_read.Mode, "Stereo")) {
-        Serial.println("Configuracion : Stereo");
-
-        queue_stereo = xQueueCreate( SIZE_STEREO_QUEUE, sizeof( struct Led_Stereo ));
-
-        if (queue_stereo == NULL) {
-          Serial.println("Error: Queue stereo not created");
-        }
-
-        if (queue_mono != NULL) {
-          vQueueDelete(queue_mono);
-          Serial.println("Successfull : Delete Queue mono");
-        }
-
-      } else {
-        Serial.println("Configuracion : Desconocida");
-      }
-
-
+      setConfig(&Config_Strip_read);
       Central_State = Led_Control;
       Strip_State = Mono;
-
       break;
-    case Led_Control:
-      if (UDP.parsePacket()) {
-        UDP.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
-        if (Is_config_queue(packetBuffer)) {
-          struct LedStripConfig Config_Strip;  // create Structure
-          strcpy(Config_Strip.Mode, "Mono"); // Data hardcode!!!
-          load_config_in_queue(&Config_Strip);
-          memset(packetBuffer, '\0', UDP_TX_PACKET_MAX_SIZE);
-          Central_State = Configuration;  //Change state to Configuration
-          Strip_State = Default;
-
-        } else {
-          Serial.print((char*)packetBuffer);
-          Serial.print(" --> ");
-          Serial.println(millis());
-          //
-        }
-        // si es config carga en queue y cambiar a estado de Config.
-        // se podria cambiar tambien el stado de Strip_state para que se apagen los leds, mientras se cambia la configuracion
-        switch (Strip_State) {
-
-          case Mono:
-            Serial.println(": Mono");
-            struct Led_Mono led_mono_data;
-            char tmp_brightness[4];
-
-            memcpy(led_mono_data.color, packetBuffer, 7);
-
-            memcpy(tmp_brightness, packetBuffer + 7, 4);
-            led_mono_data.brightness = atoi(tmp_brightness);
-            if (xQueueSend(queue_mono, &led_mono_data, ( TickType_t ) 10 ) == pdPASS) {
-              Serial.println("Successful: Color and brightness load in queue mono");
-            } else {
-              Serial.println("Error: Color and brightness NO load in queue mono");
-            }
-
-
-
-
-            memset(packetBuffer, '\0', UDP_TX_PACKET_MAX_SIZE);
-
-            break;
-          case Stereo:
-            Serial.println(": Stereo");
-            //lectura unicamente los datos ?
-            //Color izq
-            //Brillo izq
-            //Color der
-            //Brillo der
-
-            //aplicar sobre led strip x2 ( canal izq y der)
-
-            break;
-        }
-      }
-
-
-      // ver la cantidad de datos que hay en la cola, en este caso se tiene 2 colas ?,
-      messagesWaiting = uxQueueMessagesWaiting(queue_mono);
-      //Serial.print("datos en la cola mono: ");
-      //Serial.println(messagesWaiting);
-      if (messagesWaiting > MAX_BUFFER_SAFE) {
-        xSemaphoreGive(binSemaphore);
-        //Serial.println("Iniciar task.. led");
-      } else {
-        // no no activate task, task is waiting
-      }
-
-
-
+    case Led_Control: // State Led Control
+    
       break;
+
+
   }
-
-
 }
 
+bool Conect_Wifi() {
+  bool res;
+  res = wm.autoConnect("AutoConnectAP", "password"); // password protected ap
+  if (!res) {
+    Serial.println("Failed to connect");
+    return false;
+  } else {
+    Serial.println("ESP32 : CONNECT ! :)");
+    return true;
+  }
+}
 
-
-boolean ConnectUDP() { // add contador regresivo 10 times intento de conexion
-  Serial.println();
-  Serial.println("Starting UDP");
-  // in UDP error, block execution
-  if (UDP.begin(localPort) != 1)
-  {
-    Serial.println("Connection failed");
-    while (true) {
-      delay(1000);
+void setConfig(struct LedStripConfig * Config) {
+  if (!strcmp(Config->Mode, "Mono")) {
+    xTaskCreate(TaskLedMono, "LED MODE MONO", 1024, NULL, 2, NULL);
+    queue_mono = xQueueCreate( SIZE_MONO_QUEUE, sizeof( struct Led_Mono ) );
+    if (queue_mono == NULL) {
+      Serial.println("Error: Queue mono not created");
     }
+
+    if (queue_stereo != NULL) {
+      vQueueDelete(queue_stereo);
+    }
+  }else if(!strcmp(Config->Mode, "Stereo")){
+    //todo create task for stereo mode
+  }else{
+    Serial.println("Configuracion : Desconocida");
   }
-  Serial.println("UDP successful");
-  return true;
+
 }
+
+void ListenClient(WiFiClient* guest, QueueHandle_t* queue) {
+  if (guest) {
+    while (guest->connected()) {
+      int i = 0;
+      while (guest->available() > 0) {
+        char c = guest->read();
+        if ( c == '\n') {
+          Serial.println((char*) data_TCPIP);// data_TCPIP=#FFEECC255\n
+
+          if (Is_config_queue(data_TCPIP)) {
+            Serial.println("Load configuration");
+            struct LedStripConfig Config_Strip;
+            strcpy(Config_Strip.Mode, "Mono"); // Data hardcode!!!
+            load_config_in_queue(&Config_Strip);
+
+            Central_State = Configuration;
+
+          }
+          memset(data_TCPIP, '\0', sizeof(data_TCPIP));
+          i = 0;
+        } else {
+          data_TCPIP[i] = c;
+          i++;
+        }
+      }
+      delay(1);
+    }
+    guest->stop();
+    //Serial.println("Client disconnected");
+  }
+}
+
 
 bool Is_config_queue(char * queue) {
   if (!strcmp (strtok(queue, ":"), "#config")) {
@@ -311,12 +228,14 @@ void load_config_in_queue(struct LedStripConfig * Config) {
     Serial.println("Error: Load data in queue config");
   } else {
     Serial.println("Successful: Load data in queue config");
+    Serial.println((char*) Config->Mode);
   }
 }
 
 void read_config_queue(struct LedStripConfig * Config) {
   if (xQueueReceive(queue_config, &Config, ( TickType_t ) 10 ) == pdPASS) {
     Serial.println("Successful : Data read from config queue");
+    Serial.println((char*) Config->Mode);
   } else {
     Serial.println("Error : Data read from config queue");
   }
@@ -343,10 +262,10 @@ void TaskLedMono(void *pvParameters) {
 
         for (int i = 0; i < NUM_LEDS_L; i++) {
           leds_L[i] = CRGB(red, green, blue);
-          
+
         }
 
-        for (int i = 0; i < NUM_LEDS_R; i++){
+        for (int i = 0; i < NUM_LEDS_R; i++) {
           leds_R[i] = CRGB(red, green, blue);
         }
         FastLED[0].showLeds(led_mono.brightness);
